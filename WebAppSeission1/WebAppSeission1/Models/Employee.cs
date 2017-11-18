be4a584af71cbb008251a8525dfdd9c90e461d9b
DESKTOP-4KP52D6\Ahmed Ortega <ahmedsniper2020@outlook.com>
2017-11-18 21:26:37 +0200

    Test2

--- WebAppSeission1/WebAppSeission1/Models/Employee.cs
+++ WebAppSeission1/WebAppSeission1/Models/Employee.cs
@@ -10,5 +10,6 @@ namespace WebAppSeission1.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
+        public int Phone { get; set; }
     }
 }
