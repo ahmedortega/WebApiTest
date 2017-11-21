b7848392d2a4b47c1bc735798ba1831916fd0a01
DESKTOP-4KP52D6\Ahmed Ortega <ahmedsniper2020@outlook.com>
2017-11-22 01:40:50 +0200

    Put and Delete Car

--- WebAppSeission1/WebAppSeission1/Controllers/CarsController.cs
+++ WebAppSeission1/WebAppSeission1/Controllers/CarsController.cs
@@ -62,5 +62,51 @@ namespace WebAppSeission1.Controllers
             }
         }
 
+        public HttpResponseMessage Delete(int ID)
+        {
+            using (CarsDBEntities _entities = new CarsDBEntities())
+            {
+                var car = _entities.Cars.FirstOrDefault(c => c.Id == ID);
+                if (car == null)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "ID not Found");
+                }
+                else
+                {
+                    _entities.Cars.Remove(car);
+                    _entities.SaveChanges();
+                    return Request.CreateResponse(HttpStatusCode.OK, car);
+                }
+            }
+        }
+
+        public HttpResponseMessage Put(int ID, Car car)
+        {
+            using (CarsDBEntities _entities = new CarsDBEntities())
+            {
+                var myCar = _entities.Cars.FirstOrDefault(c => c.Id == ID);
+                try
+                {
+                    myCar.Name = car.Name;
+                    myCar.ModelYear = car.ModelYear;
+                    myCar.Color = car.Color;
+                    _entities.SaveChanges();
+                    var msg = Request.CreateResponse(HttpStatusCode.Created, car);
+                    msg.Headers.Location = new Uri(Request.RequestUri + "" + car.Id);
+                    return msg;
+                }
+                catch(Exception ex)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+                }
+            }
+        }
+
+
+
+
+
+
+
     }
 }
